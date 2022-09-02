99f88f5df2319b41b57860c4abbabf97f6b23f60
ChrisBaoCC <cbao23@concordcarlisle.org>
2022-09-02 11:50:53 -0400

    1 sep update

--- runner.py
+++ runner.py
@@ -5,6 +5,7 @@ from psychopy.visual import Window, TextBox2
 from psychopy.visual.rect import Rect
 from psychopy.event import Mouse
 from psychopy.core import Clock
+from psychopy.clock import CountdownTimer
 
 # CONSTANTS #
 # window size
@@ -12,6 +13,7 @@ WIDTH = 1920
 HEIGHT = 1080
 
 # length of experiment (in seconds)
+# TODO: implement stopping
 DURATION = 25 * 60
 # number of times key must be pressed to trigger effect
 TRIGGER_AMTS = (100, 10, 10)
@@ -24,6 +26,9 @@ BTNS_ENABLED = (True, True, True)
 
 WHITE = (1, 1, 1)
 BLACK = (-1, -1, -1)
+GREEN = (-1, 0.5, -1)
+BLUE = (-1, -1, 1)
+RED = (1, -1, -1)
 LINE_WIDTH = 2
 
 PHASE_INTRO = 0
@@ -51,7 +56,6 @@ BOX_ARGS = {
 }
 TEXT_ARGS = {
     "units": "pix",
-    "color": BLACK,
 }
 
 HEADER_SIZE = (1800, 100)
@@ -71,7 +75,7 @@ BTN_FONTSIZE = 30
 CONNECT_SIZE = (800, 100)
 CONNECT_POS = (0, 0)
 CONNECT_FONTSIZE = 30
-CONNECT_DELAY = 5 # connection delay (fake)
+CONNECT_DELAY = 5  # connection delay (fake)
 
 POINTS_LABEL_POS = (-100, 250)
 POINTS_COUNTER_POS = (100, 250)
@@ -84,9 +88,9 @@ PRESS_LABEL_POS = (-100, -250)
 PRESS_COUNTER_POS = (100, -250)
 LABEL_FONTSIZE = 30
 COUNTER_FONTSIZE = 40
-COUNTER_FONTSIZE_BIG = 50
+COUNTER_FONTSIZE_BIG = 80
 BUTTON_FONTSIZE = 200
-BUTTON_FONTSIZE_BIG = 250
+BUTTON_FONTSIZE_BIG = 300
 
 # GLOBALS #
 phase = PHASE_INTRO
@@ -95,6 +99,9 @@ state = STATE_INTRO
 exp_clock = Clock()
 protect_clock = Clock()
 provoke_clock = Clock()
+points_timer = CountdownTimer(0) # track time to size & color reset
+block_timer = CountdownTimer(0) # track time to allow buttons again
+blocked = False
 # track if point has been taken since the beginning or
 # the last protection interval
 provoked = False
@@ -116,23 +123,23 @@ window = Window((WIDTH, HEIGHT), color=WHITE, fullscr=True, units="pix")
 header_box = Rect(win=window, size=HEADER_SIZE, pos=HEADER_POS,
                   fillColor=WHITE, autoDraw=True, **BOX_ARGS)
 header_text = TextBox2(win=window, pos=HEADER_POS, bold=True,
-                       text="",
+                       text="", color=BLACK,
                        letterHeight=HEADER_FONTSIZE,
                        alignment="center", autoDraw=True,
                        **TEXT_ARGS)
 # full width
 desc_text1 = TextBox2(win=window, pos=DESC1_POS, bold=True,
-                      text="", size=[1600, None],
+                      text="", size=[1600, None], color=BLACK,
                       letterHeight=DESC_FONTSIZE, anchor="top_left",
                       alignment="top_left", autoDraw=True, **TEXT_ARGS)
 # right
 desc_text2 = TextBox2(win=window, pos=DESC2_POS, bold=True,
-                      text="",
+                      text="", color=BLACK,
                       letterHeight=DESC_FONTSIZE, anchor="top_left",
                       alignment="top_left", autoDraw=True, **TEXT_ARGS)
 # bottom
 desc_text3 = TextBox2(win=window, pos=DESC3_POS, bold=True,
-                      text="", size=[1600, None],
+                      text="", size=[1600, None], color=BLACK,
                       letterHeight=DESC_FONTSIZE, anchor="top_left",
                       alignment="top_left", autoDraw=True, **TEXT_ARGS)
 # forward/back
@@ -142,12 +149,12 @@ back_text = TextBox2(win=window, pos=BACK_POS,
                      text="Press [backspace] for previous page",
                      letterHeight=BTN_FONTSIZE,
                      alignment="center", autoDraw=True,
-                     **TEXT_ARGS)
+                     color=BLACK, **TEXT_ARGS)
 fwd_box = Rect(win=window, size=BTN_SIZE, pos=FWD_POS,
                fillColor=WHITE, autoDraw=True, **BOX_ARGS)
 fwd_text = TextBox2(win=window, pos=FWD_POS,
                     text="Press [space] for next page",
-                    letterHeight=BTN_FONTSIZE,
+                    letterHeight=BTN_FONTSIZE, color=BLACK,
                     alignment="center", autoDraw=True,
                     **TEXT_ARGS)
 intro_stims = [
@@ -169,29 +176,45 @@ connect_text = TextBox2(win=window, pos=CONNECT_POS, bold=True,
                         text="Connecting to server . . . please wait.",
                         letterHeight=CONNECT_FONTSIZE,
                         alignment="center", autoDraw=False,
-                        **TEXT_ARGS)
+                        color=BLACK, **TEXT_ARGS)
 connect_stims = [connect_box, connect_text]
 connect_start_time: float
 
 # play stims
 points_text = TextBox2(win=window, pos=POINTS_LABEL_POS,
-                    text="Points:", letterHeight=LABEL_FONTSIZE,
-                    alignment="center", autoDraw=False, **TEXT_ARGS)
+                       text="Points:", letterHeight=LABEL_FONTSIZE,
+                       alignment="center", autoDraw=False, color=BLACK,
+                       **TEXT_ARGS)
 points_counter = TextBox2(win=window, pos=POINTS_COUNTER_POS,
-                    text="0", letterHeight=COUNTER_FONTSIZE,
-                    alignment="center", autoDraw=False, **TEXT_ARGS)
+                          text="0", letterHeight=COUNTER_FONTSIZE,
+                          alignment="center", autoDraw=False, color=BLACK,
+                          **TEXT_ARGS)
+
+points_counter_big = TextBox2(win=window, pos=POINTS_COUNTER_POS,
+                          text="0", letterHeight=COUNTER_FONTSIZE_BIG,
+                          alignment="center", autoDraw=False, color=GREEN,
+                          **TEXT_ARGS)
+
 points = 0
 buttons = [TextBox2(win=window, pos=BUTTON_POS[i], text=chr(ord('A')+i),
                     letterHeight=BUTTON_FONTSIZE, alignment="center",
-                    autoDraw=False, **TEXT_ARGS) for i in range(3)]
+                    autoDraw=False, color=BLACK, **TEXT_ARGS)
+                    for i in range(3)]
+buttons_big = [TextBox2(win=window, pos=BUTTON_POS[i], text=chr(ord('A')+i),
+                    letterHeight=BUTTON_FONTSIZE_BIG, alignment="center",
+                    autoDraw=False, color=BLUE, **TEXT_ARGS)
+                    for i in range(3)]
 press_text = TextBox2(win=window, pos=PRESS_LABEL_POS,
-                    text="Button presses:", letterHeight=LABEL_FONTSIZE,
-                    alignment="center", autoDraw=False, **TEXT_ARGS)
+                      text="Button presses:", letterHeight=LABEL_FONTSIZE,
+                      alignment="center", autoDraw=False, color=BLACK,
+                      **TEXT_ARGS)
 press_counter = TextBox2(win=window, pos=PRESS_COUNTER_POS,
-                    text="0", letterHeight=COUNTER_FONTSIZE,
-                    alignment="center", autoDraw=False, **TEXT_ARGS)
+                         text="0", letterHeight=COUNTER_FONTSIZE,
+                         alignment="center", autoDraw=False, color=BLACK,
+                         **TEXT_ARGS)
 presses = 0
 play_stims = [points_text, points_counter, press_text, press_counter]
+
 
 def refresh_text():
     if phase == PHASE_INTRO:
@@ -228,27 +251,43 @@ def start_play_phase():
         if BTNS_ENABLED[i]:
             buttons[i].autoDraw = True
 
+
 def add_press():
     global presses
     presses += 1
     press_counter.text = str(presses)
 
+
 def check_trigger():
-    global state, presses, points
+    global state, presses, points, points_timer, block_timer, blocked
     if state == STATE_A and presses == TRIGGER_AMTS[0]:
         presses = 0
         press_counter.text = str(presses)
         points += 1
         points_counter.text = str(points)
-        # TODO: make points counter bigger
+        points_counter.autoDraw = False
+        points_counter_big.text = str(points)
+        points_counter_big.autoDraw = True
+        points_timer = CountdownTimer(1)
+
+        for button in buttons_big:
+            button.autoDraw = False
+        
+        block_timer = CountdownTimer(1)
+        blocked = True
         state = STATE_CHOOSE
     elif state == STATE_B and presses == TRIGGER_AMTS[1]\
-      or state == STATE_C and presses == TRIGGER_AMTS[2]:
+            or state == STATE_C and presses == TRIGGER_AMTS[2]:
         presses = 0
         press_counter.text = str(presses)
+
+        for button in buttons_big:
+            button.autoDraw = False
+        blocked = True
         # TODO: protection interval
+        block_timer = CountdownTimer(1)
         state = STATE_CHOOSE
-    
+
 
 def handle_keys(keys):
     global phase, state
@@ -269,31 +308,45 @@ def handle_keys(keys):
             state += 1
             refresh_text()
     elif phase == PHASE_PLAY:
-        if state == STATE_CHOOSE:
-            # TODO: make letter bigger, others disappear
-            # TODO: make enabling/disabling buttons work
-            if "a" in keys:
+        if state == STATE_CHOOSE and not blocked:
+            if "a" in keys and BTNS_ENABLED[0]:
                 state = STATE_A
+                for button in buttons:
+                    button.autoDraw = False
+                buttons_big[0].autoDraw = True
                 add_press()
-            elif "b" in keys:
+            elif "b" in keys and BTNS_ENABLED[1]:
                 print("b")
                 state = STATE_B
+                for button in buttons:
+                    button.autoDraw = False
+                buttons_big[1].autoDraw = True
                 add_press()
-            elif "c" in keys:
+            elif "c" in keys and BTNS_ENABLED[2]:
                 state = STATE_C
+                for button in buttons:
+                    button.autoDraw = False
+                buttons_big[2].autoDraw = True
                 add_press()
         elif state == STATE_A and "a" in keys\
-          or state == STATE_B and "b" in keys\
-          or state == STATE_C and "c" in keys:
+                or state == STATE_B and "b" in keys\
+                or state == STATE_C and "c" in keys:
             add_press()
             check_trigger()
-
 
 
 refresh_text()
 
 # MAINLOOP #
 while True:
+    if phase == PHASE_PLAY and points_timer.getTime() < 0.0001:
+        points_counter.autoDraw = True
+        points_counter_big.autoDraw = False
+    if blocked and block_timer.getTime() < 0.0001:
+        for button in buttons:
+            button.autoDraw = True
+        blocked = False
+
     keys = event.getKeys()
     if "escape" in keys:
         output.close()
@@ -302,7 +355,7 @@ while True:
         handle_keys(keys)
     elif phase == PHASE_CONNECT:
         if core.monotonicClock.getTime() >= connect_start_time\
-                                          + CONNECT_DELAY:
+                + CONNECT_DELAY:
             start_play_phase()
     elif phase == PHASE_PLAY:
         handle_keys(keys)
